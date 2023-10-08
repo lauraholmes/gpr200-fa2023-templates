d47f070b378168474a6670ea9c358ee606a18f3d
lauraholmes <143751116+lauraholmes@users.noreply.github.com>
2023-10-08 14:44:12 -0400

    I cant test it on my computer

--- assignments/assignment4_transformations/assets/vertexShader.vert
+++ assignments/assignment4_transformations/assets/vertexShader.vert
@@ -2,9 +2,15 @@
 layout(location = 0) in vec3 vPos;
 layout(location = 1) in vec3 vNormal;
 
+out vec4 _Model;
 out vec3 Normal;
 
 void main(){
 	Normal = vNormal;
 	gl_Position = vec4(vPos,1.0);
+
+	gl_Position = _Model * vec4(vPos,1.0);
+
+	//Convert from RHS to LHS
+	gl_Position.z*=-1.0;
 }
--- assignments/assignment4_transformations/main.cpp
+++ assignments/assignment4_transformations/main.cpp
@@ -4,6 +4,12 @@
 #include <ew/external/glad.h>
 #include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
+#include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
@@ -11,6 +17,16 @@
 #include <ew/shader.h>
 #include <ew/ewMath/vec3.h>
 #include <ew/procGen.h>
+
+#include <meh/transformations.h>
+
+myLib::Transform cubeTransforms[4]{
+	{0.5f, 0.5f, 0.0f},
+	{-0.5f, -0.5f, 0.0f},
+	{-0.5f, 0.5f, 0.0f},
+	{0.5f - 0.5f, 0.0f}
+};
+
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 
@@ -52,10 +68,13 @@ int main() {
 	glEnable(GL_DEPTH_TEST);
 
 	ew::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
-	
+
 	//Cube mesh
 	ew::Mesh cubeMesh(ew::createCube(0.5f));
-	
+	ew::Mesh cubeMesh2(ew::createCube(0.5f));
+	ew::Mesh cubeMesh3(ew::createCube(0.5f));
+	ew::Mesh cubeMesh4(ew::createCube(0.5f));
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
@@ -80,6 +99,26 @@ int main() {
 
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			for (size_t i = 0; i < 4; i++)
+			{
+				ImGui::PushID(i);
+				if (ImGui::CollapsingHeader("Transform")) {
+					ImGui::DragFloat3("Position", &cubeTransforms[i].position.x, 0.05f);
+					ImGui::DragFloat3("Rotation", &cubeTransforms[i].rotation.x, 1.0f);
+					ImGui::DragFloat3("Scale", &cubeTransforms[i].scale.x, 0.05f);
+				}
+				ImGui::PopID();
+			}
+
+			shader.use();
+			shader.setMat4("_Model", cubeTransforms[0].getModelMatrix());
+			shader.setMat4("_Model", cubeTransforms[1].getModelMatrix());
+			shader.setMat4("_Model", cubeTransforms[2].getModelMatrix());
+			shader.setMat4("_Model", cubeTransforms[3].getModelMatrix());
+			cubeMesh.draw();
+			cubeMesh2.draw();
+			cubeMesh3.draw();
+			cubeMesh4.draw();
 		}
 
 		glfwSwapBuffers(window);
@@ -91,4 +130,3 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
-
