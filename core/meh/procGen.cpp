972a041214c5e9b22296981ff898d4c90fa55644
lauraholmes <laura.holmes@mymail.champlain.edu>
2023-11-02 15:47:17 -0400

    vertices

---
+++ core/meh/procGen.cpp
@@ -0,0 +1,21 @@
+#include "../meh/procGen.h"
+#include "../ew/mesh.h"
+#include <stdlib.h>
+
+namespace yourLib {
+	ew::MeshData createSphere(float radius, int numSegments)
+	{
+
+	}
+	ew::MeshData createCylinder(float height, float radius, int numSegments)
+	{
+		float topY = height / 2;
+		float bottomY = -topY;
+
+		
+	}
+	ew::MeshData createPlane(float width, float height, int subdivisions)
+	{
+		
+	}
+}
---
+++ core/meh/procGen.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../ew/mesh.h"
+
+
+namespace myLib {
+	ew::MeshData createSphere(float radius, int numSegments);
+	ew::MeshData createCylinder(float height, float radius, int numSegments);
+	ew::MeshData createPlane(float width, float height, int subdivisions);
+}
